6925c66260f41710430d3b3846a16831efa34b7a
LunarTides <31688109+LunarTides@users.noreply.github.com>
2024-10-29 12:06:22 +0100

    Split tileset into its own file

--- Scripts/Tile.gd
+++ Scripts/Tile.gd
@@ -11,14 +11,16 @@ static func create(source_id: int, coords: Vector2i) -> void:
 
 static func dig(coords: Vector2i) -> void:
 	var tile_data := Game.tiles_root.get_cell_tile_data(coords)
+	Game.tiles_root.erase_cell(coords)
 	
-	if tile_data:
-		var item_to_drop: ItemData = tile_data.get_custom_data("ItemToDrop")
-		
-		if item_to_drop:
-			Game.tiles_root.erase_cell(coords)
-			
-			var item: Item = ITEM.instantiate()
-			item.data = item_to_drop
-			item.global_position = coords * 16
-			Game.world.add_child(item)
+	if not tile_data:
+		return
+	
+	var item_to_drop: ItemData = tile_data.get_custom_data("ItemToDrop")
+	if not item_to_drop:
+		return
+	
+	var item: Item = ITEM.instantiate()
+	item.data = item_to_drop
+	item.global_position = coords * 16
+	Game.world.add_child(item)
